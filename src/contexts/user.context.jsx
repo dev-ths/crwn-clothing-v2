bb796457eefd4c73647ad7135305624c54c6c4b7
dev-ths <alphavo.ths@gmail.com>
2024-02-23 11:13:13 -0800

    implemented useReducer for userContext & cartContext

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,29 +1,59 @@
-import { createContext, useState, useEffect } from 'react';
+import { createContext, useEffect, useReducer } from "react"
+import { createAction } from "../utils/reducer/reducer.utils"
 
 import {
-  onAuthStateChangedListener,
-  createUserDocumentFromAuth,
-} from '../utils/firebase/firebase.utils';
+	onAuthStateChangedListener,
+	createUserDocumentFromAuth,
+} from "../utils/firebase/firebase.utils"
 
 export const UserContext = createContext({
-  setCurrentUser: () => null,
-  currentUser: null,
-});
+	setCurrentUser: () => null,
+	currentUser: null,
+})
+
+export const USER_ACTION_TYPES = {
+	SET_CURRENT_USER: "SET_CURRENT_USER",
+}
+
+const userReducer = (state, action) => {
+	const { type, payload } = action
+
+	switch (type) {
+		case "SET_CURRENT_USER":
+			return {
+				...state,
+				currentUser: payload,
+			}
+
+		default:
+			throw new Error(`Unhandled type ${type} in userReducer`)
+	}
+}
+
+const INITIAL_STATE = {
+	currentUser: null,
+}
 
 export const UserProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null);
-  const value = { currentUser, setCurrentUser };
+	const [state, dispatch] = useReducer(userReducer, INITIAL_STATE)
+	const { currentUser } = state
 
-  useEffect(() => {
-    const unsubscribe = onAuthStateChangedListener((user) => {
-      if (user) {
-        createUserDocumentFromAuth(user);
-      }
-      setCurrentUser(user);
-    });
+	const setCurrentUser = (user) => {
+		dispatch(createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user))
+	}
 
-    return unsubscribe;
-  }, []);
+	const value = { currentUser, setCurrentUser }
 
-  return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
-};
+	useEffect(() => {
+		const unsubscribe = onAuthStateChangedListener((user) => {
+			if (user) {
+				createUserDocumentFromAuth(user)
+			}
+			setCurrentUser(user)
+		})
+
+		return unsubscribe
+	}, [])
+
+	return <UserContext.Provider value={value}>{children}</UserContext.Provider>
+}
